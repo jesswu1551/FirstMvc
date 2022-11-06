51c09bd12e0c14fa991ad947319de65f3964cdba
jess.wu <jesswu1551@gmail.com>
2022-11-06 20:39:05 +0800

    add friends sample

---
+++ MVC_TEST/Controllers/FriendsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MVC_TEST.Models;
+
+namespace MVC_TEST.Controllers
+{
+    public class FriendsController : Controller
+    {
+        private FriendsContext db = new FriendsContext();
+
+        // GET: Friends
+        public ActionResult Index()
+        {
+            return View(db.FriendsModels.ToList());
+        }
+
+        // GET: Friends/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            FriendsModel friendsModel = db.FriendsModels.Find(id);
+            if (friendsModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(friendsModel);
+        }
+
+        // GET: Friends/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Friends/Create
+        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
+        // 詳細資訊，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Phone,Email,City")] FriendsModel friendsModel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.FriendsModels.Add(friendsModel);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(friendsModel);
+        }
+
+        // GET: Friends/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            FriendsModel friendsModel = db.FriendsModels.Find(id);
+            if (friendsModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(friendsModel);
+        }
+
+        // POST: Friends/Edit/5
+        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
+        // 詳細資訊，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Phone,Email,City")] FriendsModel friendsModel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(friendsModel).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(friendsModel);
+        }
+
+        // GET: Friends/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            FriendsModel friendsModel = db.FriendsModels.Find(id);
+            if (friendsModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(friendsModel);
+        }
+
+        // POST: Friends/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            FriendsModel friendsModel = db.FriendsModels.Find(id);
+            db.FriendsModels.Remove(friendsModel);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MVC_TEST/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_TEST.Controllers
+{
+    public class HomeController : Controller
+    {
+        [AllowAnonymous]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            if (User.Identity.Name != "john@gmail.com")
+            {
+                return Content($"{User.Identity.Name}帳號無權限存取此 Action 動作方法");
+            }
+
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        [Authorize(Users = "kevin4267@gmail.com, mary@gmail.com")]
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [Authorize(Roles = "Admin, Supervisor")]
+        public ActionResult Administrators()
+        {
+            return View();
+        }
+    }
+}
