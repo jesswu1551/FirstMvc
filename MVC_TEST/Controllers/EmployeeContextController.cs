6ddb6b494c8ec9da16034c03f06dbccb069e35c6
jess.wu <jesswu1551@gmail.com>
2022-11-06 21:36:28 +0800

    add employee data by database initializer

---
+++ MVC_TEST/Controllers/EmployeeContextController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MVC_TEST.Models;
+
+namespace MVC_TEST.Controllers
+{
+    public class EmployeeContextController : Controller
+    {
+        private EmployeeContext db = new EmployeeContext();
+
+        // GET: EmployeeContext
+        public ActionResult Index()
+        {
+            return View(db.Employees.ToList());
+        }
+
+        // GET: EmployeeContext/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Employee employee = db.Employees.Find(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+        // GET: EmployeeContext/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: EmployeeContext/Create
+        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
+        // 詳細資訊，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Phone,Email,Department,Title")] Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Employees.Add(employee);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(employee);
+        }
+
+        // GET: EmployeeContext/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Employee employee = db.Employees.Find(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+        // POST: EmployeeContext/Edit/5
+        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
+        // 詳細資訊，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Phone,Email,Department,Title")] Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(employee).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(employee);
+        }
+
+        // GET: EmployeeContext/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Employee employee = db.Employees.Find(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+        // POST: EmployeeContext/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Employee employee = db.Employees.Find(id);
+            db.Employees.Remove(employee);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MVC_TEST/Models/EmployeeInitializer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MVC_TEST.Models
+{
+    public class EmployeeInitializer : DropCreateDatabaseIfModelChanges<EmployeeContext>
+    {
+        protected override void Seed(EmployeeContext empContext)
+        {
+            //step1 以 List 建立員工資料
+            List<Employee> employees = new List<Employee>
+            {
+
+new Employee { Id = 3, Name = "", Phone = "", Email = "", Department = "", Title = "" },
+new Employee { Id = 4, Name = "", Phone = "", Email = "", Department = "", Title = "" },
+new Employee { Id = 5, Name = "", Phone = "", Email = "", Department = "", Title = "" },
+new Employee { Id = 6, Name = "", Phone = "", Email = "", Department = "", Title = "" },
+new Employee { Id = 7, Name = "", Phone = "", Email = "", Department = "", Title = "" }
+            };
+
+            //step2 將 List 每筆資料加入 Entity Framework 的 Employee 中
+            employees.ForEach(x => empContext.Employees.Add(x));
+
+            //step3 儲存異動
+            empContext.SaveChanges();
+        }
+    }
+}
