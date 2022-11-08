98481410a8f029ce407164631a69a6f962054b25
jess.wu <jesswu1551@gmail.com>
2022-11-08 22:24:54 +0800

    create simple_card_partial view

---
+++ MVC_TEST/Controllers/PartialViewController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_TEST.Controllers
+{
+    public class PartialViewController : Controller
+    {
+        // GET: PartialView
+        public ActionResult SimpleCard()
+        {
+            return View();
+        }
+    }
+}
